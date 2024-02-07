edf9720414042536cf2caabab4e11cbd7549cfad
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2024-02-07 13:43:41 +0500

    ReverseProxy Settings added and packages removed

--- Identification/Program.cs
+++ Identification/Program.cs
@@ -1,10 +1,13 @@
 global using FastEndpoints;
+using FastEndpoints.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
 
+app.UseSwaggerGen();
 app.UseHttpsRedirection();
 app.Run();
--- Marketplace/Program.cs
+++ Marketplace/Program.cs
@@ -1,11 +1,14 @@
 global using FastEndpoints;
+using FastEndpoints.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
 
+app.UseSwaggerGen();
 app.UseHttpsRedirection();
 
 app.Run();
