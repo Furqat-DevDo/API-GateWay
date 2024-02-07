c627070230e5651c8ee17765b27c463f9af12125
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2024-02-07 18:38:22 +0500

    Some changes done

--- Identification/Features/Admin/CreateUser/Data.cs
+++ Identification/Features/Admin/CreateUser/Data.cs
@@ -1,6 +1,12 @@
-﻿namespace Identification.Features.Admin.CreateUser;
+﻿using Identification.Entities;
+
+namespace Identification.Features.Admin.CreateUser;
 
 public static class Data
 {
-
+      
+      public static async Task<User> CreateUser(User entity)
+      {
+            return entity;
+      } 
 }
